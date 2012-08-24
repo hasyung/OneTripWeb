156c8a32bacd11f77f2895aa4bab6ebd86b3368e
ChuckLee <sapronlee@gmail.com>
2012-08-24 10:25:28 +0800

    新建项目
    新建OneTripWeb项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OneTripWeb::Application.config.secret_token = '28ec33998bbaacad0ea8918399794e26bebee13c5dd0cdcb73fbc4ec5d4e3f1be6f38ae32659e2cde9ecb6d5c05c96d1d056f43cc21c9cf677c018e93f4d733d'
