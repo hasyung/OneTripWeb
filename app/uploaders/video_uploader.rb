b53b3f96ce46105e3c3aededdf80e5ec98e7745e
hasyung <yh8102326@gmail.com>
2012-09-14 10:40:38 +0800

    remove cached database.yml

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,1 +1,9 @@
 # encoding: utf-8
+
+class VideoUploader < CarrierWave::Uploader::Base
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+  
+  include CarrierWave::MiniMagick
+  include CarrierWave::MimeTypes
+
+end
