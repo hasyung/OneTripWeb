562e478656a1dd247b5c3d27b30e62b45a666625
hasyung <yh8102326@gmail.com>
2012-09-19 17:48:20 +0800

    修改info小贴士

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -5,6 +5,10 @@ class Admin::ArticlesController < Admin::ApplicationController
 	def new
 		@article = @place.articles.new params[:article]
 	end
+  
+ def show
+  @article = Article.find params[:id]
+ end
 
 	def create
 		@article = @place.articles.new params[:article]
--- app/uploaders/video_cover_uploader.rb
+++ app/uploaders/video_cover_uploader.rb
@@ -19,7 +19,11 @@ class VideoCoverUploader < CarrierWave::Uploader::Base
   process :set_content_type
   
   version :thumb do
-    process :resize_to_limit => [250, 150]
+    process :resize_to_limit => [510, 300]
+  end
+  
+  version :thumb_small do
+    process :resize_to_fill => [250, 150]
   end
 
   def extension_white_list
