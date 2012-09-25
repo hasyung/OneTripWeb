56ca1da878fc7b21fc8480987b746170b6453622
hasyung <yh8102326@gmail.com>
2012-09-25 11:17:44 +0800

    测试样式2

--- app/models/province.rb
+++ app/models/province.rb
@@ -6,15 +6,15 @@ class Province < ActiveRecord::Base
 
   # Validates
   validates :name, :key, :presence => true
-     with_options :if => :name? do |name|
-     name.validates :name, :length => { :within => 2..30 }
-     name.validates :name, :uniqueness => true
-  end
+  with_options :if => :name? do |name|
+    name.validates :name, :length => { :within => 2..30 }
+    name.validates :name, :uniqueness => true
+end
   with_options :if => :key? do |key|
-  	key.validates :key, :uniqueness => true
-  	key.validates :key, :format => { :with => /^[A-Za-z0-9\s]+$/ }
-  	key.validates :key, :length => { :within => 2..30 }
-  end
+    key.validates :key, :uniqueness => true
+    key.validates :key, :format => { :with => /^[A-Za-z0-9\s]+$/ }
+    key.validates :key, :length => { :within => 2..30 }
+end
 
   # Scopes
   scope :search_name, lambda { |name| where("ucase(`provinces`.`name`) like concat('%',ucase(?),'%')", name) }
