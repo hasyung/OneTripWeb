f590208d42aa508ed33eaf0296efde7bbe01098a
ChuckLee <sapronlee@gmail.com>
2012-09-25 11:28:46 +0800

    修改province model的格式

--- app/models/province.rb
+++ app/models/province.rb
@@ -6,22 +6,16 @@ class Province < ActiveRecord::Base
 
   # Validates
   validates :name, :key, :presence => true
-<<<<<<< HEAD
+
   with_options :if => :name? do |name|
     name.validates :name, :length => { :within => 2..30 }
     name.validates :name, :uniqueness => true
-end
-=======
-    with_options :if => :name? do |name|
-    name.validates :name, :length => { :within => 2..30 }
-    name.validates :name, :uniqueness => true
   end
->>>>>>> 添加超级管理员
   with_options :if => :key? do |key|
     key.validates :key, :uniqueness => true
     key.validates :key, :format => { :with => /^[A-Za-z0-9\s]+$/ }
     key.validates :key, :length => { :within => 2..30 }
-end
+  end
 
   # Scopes
   scope :search_name, lambda { |name| where("ucase(`provinces`.`name`) like concat('%',ucase(?),'%')", name) }
