e4afd4ff74efdc3f9cba7b5b07b3e5c25518d5e3
hasyung <yh8102326@gmail.com>
2012-10-15 18:33:32 +0800

    修正设置模块权限写入方式

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -4,15 +4,15 @@ class Admin::SettingsController < Admin::ApplicationController
   helper_method :permission
 	
   def index
-		if request.post?
+     if request.post?
 			if !params[:setting].blank?
 				Setting.app_name = params[:setting][:app_name]
 				Setting.app_keywords = params[:setting][:app_keywords]
 				Setting.app_description = params[:setting][:app_description]
 			end
 			redirect_to :admin_root, :notice => t("helpers.messages.setting")
+    end
 		end
-	end
   
   private
 	def self.permission
